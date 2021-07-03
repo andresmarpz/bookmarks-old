e0a7d41ad947ebad8d33987060e13c0c8461d464
andresmarpz <andresmarpz@gmail.com>
2021-07-02 22:03:23 -0300

    Changed urls

--- src/management/Store.tsx
+++ src/management/Store.tsx
@@ -1,165 +1,185 @@
-import axios from 'axios';
-import Cookies from 'js-cookie';
-import React, { useEffect, useReducer, useState } from 'react';
-import Card from '../components/global/Card';
-import { apiUrl, domain } from '../helper/Constants';
-import isDev from '../helper/Environment';
- 
-interface Card{
-    title: string,
-    link: string,
-    description?: string,
-    collection: string,
-    id: string
+import axios from "axios";
+import Cookies from "js-cookie";
+import React, { useEffect, useReducer, useState } from "react";
+import Card from "../components/global/Card";
+import { apiUrl, domain } from "../helper/Constants";
+import isDev from "../helper/Environment";
+
+interface Card {
+	title: string;
+	link: string;
+	description?: string;
+	collection: string;
+	id: string;
 }
 
-interface User{
-    username: string,
-    id: string
+interface User {
+	username: string;
+	id: string;
 }
 
-interface Collection{
-    label: string
+interface Collection {
+	label: string;
 }
 
 const defCollection: Collection = {
-    label: 'General'
-}
-
-const initialState = {
-    sidebar: false,
-    authenticated: false,
-    new: false,
-    userMenu: false,
-    user: {} as User,
-    collections: [] as string[],
-    collection: defCollection,
-    cards: [] as Card[]
+	label: "General",
 };
 
-type actionType = 
-    | { type: 'authenticate', payload: User }
-    | { type: 'add-card', payload: Card }
-    | { type: 'add-dummy', payload: Collection }
-    | { type: 'add-collection', payload: string }
-    | { type: 'change-collection', payload: Collection }
-    | { type: 'replace-dummy', payload: Card }
-    | { type: 'remove-card', payload: string }
-    | { type: 'toggle-menu' }
-    | { type: 'toggle-sidebar' }
-    | { type: 'toggle-new' }
+const initialState = {
+	sidebar: false,
+	authenticated: false,
+	new: false,
+	userMenu: false,
+	user: {} as User,
+	collections: [] as string[],
+	collection: defCollection,
+	cards: [] as Card[],
+};
+
+type actionType =
+	| { type: "authenticate"; payload: User }
+	| { type: "add-card"; payload: Card }
+	| { type: "add-dummy"; payload: Collection }
+	| { type: "add-collection"; payload: string }
+	| { type: "change-collection"; payload: Collection }
+	| { type: "replace-dummy"; payload: Card }
+	| { type: "remove-card"; payload: string }
+	| { type: "toggle-menu" }
+	| { type: "toggle-sidebar" }
+	| { type: "toggle-new" };
 
 const reducer = (state: typeof initialState, action: actionType) => {
-    switch(action.type){
-        case 'authenticate':
-            return {...state, authenticated: true, user: action.payload };
-        case 'replace-dummy':
-            const index = state.cards.findIndex(card => card.id === 'dummy');
-            state.cards.splice(index, 1, action.payload);
+	switch (action.type) {
+		case "authenticate":
+			return { ...state, authenticated: true, user: action.payload };
+		case "replace-dummy":
+			const index = state.cards.findIndex((card) => card.id === "dummy");
+			state.cards.splice(index, 1, action.payload);
 
-            return {...state};
-        case 'add-dummy':
-            const card: Card = {
-                title: 'Loading..',
-                description: '...',
-                link: '',
-                collection: action.payload.label,
-                id: 'dummy'
-            };
+			return { ...state };
+		case "add-dummy":
+			const card: Card = {
+				title: "Loading..",
+				description: "...",
+				link: "",
+				collection: action.payload.label,
+				id: "dummy",
+			};
 
-            return {...state, cards: [...state.cards, card]};
-        case 'add-card':
-            return {...state, cards: [...state.cards, action.payload]};
-        case 'add-collection':
-            return {...state, collections: [...state.collections, action.payload]};
-        case 'change-collection':
-            return {...state, collection: action.payload};
-        case 'remove-card':
-            const cards = state.cards.filter(card => card.id !== action.payload);
-            return {...state, cards: cards};
-        case 'toggle-menu':
-            return {...state, userMenu: !state.userMenu};
-        case 'toggle-sidebar':
-            return {...state, sidebar: !state.sidebar};
-        case 'toggle-new':
-            return {...state, new: !state.new};
-    }
+			return { ...state, cards: [...state.cards, card] };
+		case "add-card":
+			return { ...state, cards: [...state.cards, action.payload] };
+		case "add-collection":
+			return {
+				...state,
+				collections: [...state.collections, action.payload],
+			};
+		case "change-collection":
+			return { ...state, collection: action.payload };
+		case "remove-card":
+			const cards = state.cards.filter(
+				(card) => card.id !== action.payload
+			);
+			return { ...state, cards: cards };
+		case "toggle-menu":
+			return { ...state, userMenu: !state.userMenu };
+		case "toggle-sidebar":
+			return { ...state, sidebar: !state.sidebar };
+		case "toggle-new":
+			return { ...state, new: !state.new };
+	}
+};
+
+export enum appStates {
+	LOADING,
+	LOADED,
 }
 
-export enum appStates{
-    LOADING, LOADED
-}
-
-interface ContextTypes{
-    state: typeof initialState;
-    dispatch: React.Dispatch<actionType>,
-    appState: appStates
+interface ContextTypes {
+	state: typeof initialState;
+	dispatch: React.Dispatch<actionType>;
+	appState: appStates;
 }
 
 export const Store = React.createContext<ContextTypes>({
-    state: initialState,
-    dispatch: () => {},
-    appState: appStates.LOADING
+	state: initialState,
+	dispatch: () => {},
+	appState: appStates.LOADING,
 });
 
-const StoreJSX = ({children}: any) => {
+const StoreJSX = ({ children }: any) => {
+	const [state, dispatch] = useReducer(reducer, initialState);
 
-    const [state, dispatch] = useReducer(reducer, initialState);
+	const [appState, setAppState] = useState(appStates.LOADING);
 
-    const [appState, setAppState] = useState(appStates.LOADING);
+	const value = { state, dispatch, appState };
 
-    const value = {state, dispatch, appState};
+	useEffect(() => {
+		Cookies.set("nextUrl", "https://bookmarks.andres.run", {
+			domain: domain,
+		});
 
-    useEffect(() => {
-        console.log(process.env.NODE_ENV);
-        console.log(isDev());
-        console.log(apiUrl);
+		axios.defaults.withCredentials = true;
+		axios.get(apiUrl + "/auth").then((res) => {
+			if (res.status === 200) {
+				if (res.data.title === "TRUE") {
+					dispatch({
+						type: "authenticate",
+						payload: {
+							username: res.data.message.username,
+							id: String(res.data.message.id),
+						},
+					});
 
-        Cookies.set('nextUri', '/bookmarks', { domain: domain });
+					const collections = axios
+						.get(apiUrl + "/get/collections")
+						.then((res) => {
+							const cols = res.data.collections;
+							Object.keys(cols).forEach((key) => {
+								dispatch({
+									type: "add-collection",
+									payload: key,
+								});
 
-        axios.defaults.withCredentials = true;
-        axios.get(apiUrl +'/auth').then(res => {
-            if(res.status === 200){
-                console.log(res);
-                if(res.data.title === 'TRUE'){
-                    dispatch({ type: 'authenticate', payload: { username: res.data.message.username, id: String(res.data.message.id) } });
+								cols[key].forEach((card: Card) => {
+									dispatch({
+										type: "add-card",
+										payload: card,
+									});
+								});
+							});
+						});
 
-                    const collections = axios.get(apiUrl +'/get/collections').then(res => {
+					const avatar = axios.get(
+						`https://avatars.githubusercontent.com/u/${res.data.message.id}?v=4`,
+						{
+							withCredentials: false,
+						}
+					);
+					const svg = axios.get("https://files.andres.run/plus.svg", {
+						withCredentials: false,
+					});
+					const arrow = axios.get(
+						"https://files.andres.run/arrow.svg",
+						{
+							withCredentials: false,
+						}
+					);
 
-                        const cols = res.data.collections;
-                        Object.keys(cols).forEach(key => {
-                            dispatch({ type: 'add-collection', payload: key });
+					Promise.allSettled([collections, avatar, svg, arrow]).then(
+						() => {
+							setAppState(appStates.LOADED);
+						}
+					);
+				} else setAppState(appStates.LOADED);
 
-                            cols[key].forEach((card: Card) => {
-                                dispatch({ type: 'add-card', payload: card });
-                            });
-                        });
-                    });
+				return res.data;
+			}
+		});
+	}, []);
 
-                    const avatar = axios.get(`https://avatars.githubusercontent.com/u/${res.data.message.id}?v=4`, {
-                        withCredentials: false
-                    });
-                    const svg = axios.get('https://andres.run/files/plus.svg', {
-                        withCredentials: false
-                    });
-                    const arrow = axios.get('https://andres.run/files/arrow.svg', {
-                        withCredentials: false
-                    });
+	return <Store.Provider value={value}>{children}</Store.Provider>;
+};
 
-                    Promise.allSettled([collections, avatar, svg, arrow]).then(() => {
-                        setAppState(appStates.LOADED);
-                    });
-
-                }else setAppState(appStates.LOADED);
-
-                return res.data;
-            }
-        });
-    }, []);
-
-    return (
-        <Store.Provider value={value}>{children}</Store.Provider>
-    );
-}
- 
-export default StoreJSX
+export default StoreJSX;
