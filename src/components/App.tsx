ae0f38503a38ceeb14033e24d76a997c233de34f
andresmarpz <andresmarpz@gmail.com>
2021-09-08 22:23:37 -0300

    Changes to auth state screens

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,29 +1,37 @@
-import { useContext } from 'react';
-import { Store, appStates } from '../management/Store';
-import Container from './container/Container';
-import Navbar from './navbar/Navbar';
-import LoadingScreen from './LoadingScreen';
-import Footer from './footer/Footer';
-import Sidebar from './sidebar/Sidebar';
-import Separator from './global/Separator';
-import New from './footer/New';
+import { useContext } from "react";
+import { Store, appStates } from "../management/Store";
+import Container from "./container/Container";
+import Navbar from "./navbar/Navbar";
+import LoadingScreen from "./LoadingScreen";
+import Footer from "./footer/Footer";
+import Sidebar from "./sidebar/Sidebar";
+import Separator from "./global/Separator";
+import New from "./footer/New";
+import LoginScreen from "./LoginScreen";
 
 const App = () => {
+	const { state, dispatch, appState } = useContext(Store);
 
-    const { state, dispatch, appState } = useContext(Store);
+	return (
+		<div className="app">
+			{state.sidebar && state.authenticated && <Sidebar />}
+			{state.sidebar && state.authenticated && (
+				<Separator vertical={true} />
+			)}
+			<div className="main">
+				{state.authenticated ? (
+					<div>
+						<Navbar />
+						<Separator vertical={false} />
+						<Container />
+						{appState === appStates.LOADING && <LoadingScreen />}
+					</div>
+				) : (
+					<LoginScreen />
+				)}
+			</div>
+		</div>
+	);
+};
 
-    return (
-        <div className='app'>
-            { state.sidebar && state.authenticated && <Sidebar />}
-            { state.sidebar && state.authenticated && <Separator vertical={true} />}
-            <div className='main'>
-                <Navbar />
-                <Separator vertical={false} />
-                <Container />
-                { appState === appStates.LOADING && <LoadingScreen /> }
-            </div>
-        </div>
-    );
-}
- 
-export default App
+export default App;
